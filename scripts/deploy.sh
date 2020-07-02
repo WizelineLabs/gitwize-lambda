3a9789b180598607433bf6c6290c042a25c2562a
raymondgao <raymond.gao@wizeline.com>
2020-07-02 13:06:13 +0700

    add prod environment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-export APP_STAGE=dev
-export PATTERN="v[0-9]+(\.[0-9]+).*"
-if [[ ${CIRCLE_TAG} =~  ${PATTERN} ]]; then
-    export APP_STAGE=qa
+if [[ -z "${APP_STAGE}" ]]; then
+    export APP_STAGE=dev
+fi
+
+if [[ ${APP_STAGE} == 'qa' ]]; then
     export DB_CONN_STRING=${DB_CONN_STRING_QA}
     export DEFAULT_GITHUB_TOKEN=${DEFAULT_GITHUB_TOKEN_QA}
     export CYPHER_PASS_PHASE=${CYPHER_PASS_PHASE_QA}
     export USE_DEFAULT_API_TOKEN=${USE_DEFAULT_API_TOKEN_QA}
     echo "deploy to QA"
+elif [[ ${APP_STAGE} == 'prod' ]]
+    export DB_CONN_STRING=${DB_CONN_STRING_PROD}
+    export DEFAULT_GITHUB_TOKEN=${DEFAULT_GITHUB_TOKEN_PROD}
+    export CYPHER_PASS_PHASE=${CYPHER_PASS_PHASE_PROD}
+    export USE_DEFAULT_API_TOKEN=${USE_DEFAULT_API_TOKEN_PROD}
+    echo "deploy to PROD"
 fi
 
 sls deploy --stage ${APP_STAGE} --verbose
