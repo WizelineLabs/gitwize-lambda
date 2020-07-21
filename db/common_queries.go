661e0a91146aff0f53f248b49c8722c30355beaf
Sang Dinh <sang.dinh@wizeline.com>
2020-07-21 10:43:22 +0700

    remove merge commit count

--- db/common_queries.go
+++ db/common_queries.go
@@ -90,6 +90,7 @@ DELETE FROM metric WHERE branch='master' AND type=4;
 INSERT INTO metric (repository_id, branch, type, value, year, month, day, hour)
 SELECT repository_id, 'master', 4, COUNT(*), year, year*100+month, (year*100+month)*100+day, (year*10000+month*100+day)*100+hour
 FROM commit_data
+WHERE num_parents=1
 GROUP BY repository_id, year, month, day, hour
 ;
 
@@ -99,6 +100,7 @@ DELETE FROM metric WHERE branch='master' AND type=2;
 INSERT INTO metric (repository_id, branch, type, value, year, month, day, hour)
 SELECT repository_id, 'master', 2, SUM(addition_loc), year, year*100+month, (year*100+month)*100+day, (year*10000+month*100+day)*100+hour
 FROM commit_data
+WHERE num_parents=1
 GROUP BY repository_id, year, month, day, hour
 ;
 
@@ -108,6 +110,7 @@ DELETE FROM metric WHERE branch='master' AND type=3;
 INSERT INTO metric (repository_id, branch, type, value, year, month, day, hour)
 SELECT repository_id, 'master', 3, SUM(deletion_loc), year, year*100+month, (year*100+month)*100+day, (year*10000+month*100+day)*100+hour
 FROM commit_data
+WHERE num_parents=1
 GROUP BY repository_id, year, month, day, hour
 ;
 
@@ -156,6 +159,7 @@ INSERT INTO metric (repository_id, branch, type, value, year, month, day, hour)
 SELECT repository_id, 'master', 4, COUNT(*), year, year*100+month, (year*100+month)*100+day, (year*10000+month*100+day)*100+hour
 FROM commit_data
 WHERE repository_id=$repoID
+AND num_parents=1
 GROUP BY repository_id, year, month, day, hour
 ;
 
@@ -166,6 +170,7 @@ INSERT INTO metric (repository_id, branch, type, value, year, month, day, hour)
 SELECT repository_id, 'master', 2, SUM(addition_loc), year, year*100+month, (year*100+month)*100+day, (year*10000+month*100+day)*100+hour
 FROM commit_data
 WHERE repository_id=$repoID
+AND num_parents=1
 GROUP BY repository_id, year, month, day, hour
 ;
 
@@ -176,6 +181,7 @@ INSERT INTO metric (repository_id, branch, type, value, year, month, day, hour)
 SELECT repository_id, 'master', 3, SUM(deletion_loc), year, year*100+month, (year*100+month)*100+day, (year*10000+month*100+day)*100+hour
 FROM commit_data
 WHERE repository_id=$repoID
+AND num_parents=1
 GROUP BY repository_id, year, month, day, hour
 ;
 
